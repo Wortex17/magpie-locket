c92fa69edec3957724c294dec425beda953eaa2b
Patrick Michael Hopf <phopf.mmt-m2013@fh-salzburg.ac.at>
2015-12-29 17:45:46 +0100

    Add lock/unlock functionality, supported with rsa-keypairs

--- sandbox/general.js
+++ sandbox/general.js
@@ -1,10 +1,11 @@
 var
     _ = require('lodash'),
     objecthash = require('object-hash'),
-    bson = require('bson')
+    bson = require('bson'),
+    NodeRSA = require('node-rsa')
     ;
 var
-    serializer = require('../lib/serializer')
+    locket = require('../lib/locket')
     ;
 
 var object = {
@@ -16,36 +17,32 @@ var object = {
         B: 'b',
         C: 'c'
     },
-    buffer: new Buffer("buffer")
+    buffer: new Buffer("buffer"),
+    dupl: {
+
+        number: 44,
+        bool: true,
+        string: "foobar",
+        obj: {
+            A: 'a',
+            B: 'b',
+            C: 'c'
+        },
+        buffer: new Buffer("buffer")
+    }
 };
 object.objAlt = object.obj;
 object.obj.A += "a";
 object.obj.UP = object;
 
-var x = {y: 4};
-x.x = x;
-var y = _.cloneDeep(x);
-y.y = 3;
-console.log(x, y);
+var locketObj = locket.createNew();
+locket.writeContent(locketObj, "test", object);
 
-var serialized = serializer.serialize(object);
+var keypair = new NodeRSA({b: 512});
 
-console.log(serialized);
-//Hash that represents the state of this object (easy comparison)
-console.log(objecthash(serialized));
-
-var BSON = new bson.BSONPure.BSON();
-var db = {"serializedField":serialized};
-var dbserialized = BSON.serialize(db);
-console.log(dbserialized);
-var dbdeserialized = BSON.deserialize(dbserialized);
-console.log("======");
-//console.log(dbdeserialized.serializedField);
-//Needs sanitation from the BSON format
-_.forEach(dbdeserialized.serializedField, function(value, key, collection){
-    if(value._bsontype == 'Binary')
-    {
-        collection[key] = value.buffer;
-    }
-});
-console.log(dbdeserialized.serializedField);
+console.log(locketObj);
+//console.log(locket.convertBSONToLocket(locket.convertLocketToBSON(locketObj)));
+var lockedLocket = locket.lock(locketObj, keypair);
+console.log(lockedLocket);
+var unlockedLocket = locket.unlock(lockedLocket, keypair);
+console.log(unlockedLocket);
